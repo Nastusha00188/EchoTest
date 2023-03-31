9f7a9e99e2e1549a8cd5da4c01d121bf734ee82d
Анастасия Киреева <nastusha00188@gmail.com>
2023-03-31 17:59:57 +0300

    Initial commit

---
+++ src/test/java/ru/netology/echo/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.echo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldReturnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo ("Hi"))
+
+        ;
+    }
+
+}
